125819a699ef58cb1491fbc3f39ed8e97662ead9
Arasal <julienh11@gmail.com>
2017-03-25 20:31:40 -0300

    Update Scripts

--- Profiles/Quester/Scripts/InteractWithHotSpots.cs
+++ Profiles/Quester/Scripts/InteractWithHotSpots.cs
@@ -18,24 +18,26 @@ int q = QuestID; /* not used but otherwise getting warning QuestID not used */
 uint baseAddress = 0;
 
 /* If Entry found continue, otherwise continue checking around HotSpots */
-if ((unit.IsValid && !nManagerSetting.IsBlackListedZone(unit.Position) && !nManagerSetting.IsBlackListed(unit.Guid)) || (node.IsValid && !nManagerSetting.IsBlackListedZone(node.Position) && !nManagerSetting.IsBlackListed(node.Guid)))
+if ((unit.IsValid && (!nManagerSetting.IsBlackListedZone(unit.Position) && !nManagerSetting.IsBlackListed(unit.Guid) || questObjective.IgnoreBlackList )) ||
+		(node.IsValid && (!nManagerSetting.IsBlackListedZone(node.Position) && !nManagerSetting.IsBlackListed(node.Guid) || questObjective.IgnoreBlackList)))
 {
-
 	/* Entry found, GoTo */
 	if(node.IsValid)
 	{
 		unit = new WoWUnit(0);
-		baseAddress = MovementManager.FindTarget(node, questObjective.Range);
+		baseAddress = MovementManager.FindTarget(node, questObjective.Range, ignoreBlacklist:questObjective.IgnoreBlackList);
 	}
 	else if(unit.IsValid)
 	{
 		node = new WoWGameObject(0);
-		baseAddress = MovementManager.FindTarget(unit, questObjective.Range);
+		baseAddress = MovementManager.FindTarget(unit, questObjective.Range, ignoreBlacklist:questObjective.IgnoreBlackList);
 	}
+
 	Thread.Sleep(500);
-	
+
 	if (MovementManager.InMovement)
         return false;
+	
 	if (questObjective.IgnoreNotSelectable)
 	{
 	    if ((node.IsValid && node.GetDistance > questObjective.Range) || (unit.IsValid && unit.GetDistance > questObjective.Range))
@@ -45,8 +47,8 @@ if ((unit.IsValid && !nManagerSetting.IsBlackListedZone(unit.Position) && !nMana
 	{
         if(baseAddress <= 0)
 	        return false;
-        if (baseAddress > 0 && ((node.IsValid && node.GetDistance > questObjective.Range) || (unit.IsValid && unit.GetDistance > questObjective.Range)))
-	    return false;
+		if (baseAddress > 0 && ((node.IsValid && node.GetDistance > questObjective.Range) || (unit.IsValid && unit.GetDistance > questObjective.Range)))
+			return false;
 	}
   
 	Thread.Sleep(100 + Usefuls.Latency); /* ZZZzzzZZZzz */
